ed5bf6613f337d24b56094ebf85ccd7171880445
Aman14sh <amasharma862@gmail.com>
2024-03-08 23:04:56 +0530

    c

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const  Footer= () =>{
+    
+    return (<footer className="Footer  bg-yellow-500 flex justify-center items-center">
+
+        <p className=" text-slate-100 font-extrabold text-center ">Aman Sharma</p>
+
+
+    </footer>)
+
+}
